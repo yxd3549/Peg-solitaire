ed068117192298ef966b8c9c7cdca78be8d60eb4
lshadler <lxs2208@rit.edu>
2017-02-11 14:13:55 -0500

    implemented basic Peg.java

--- src/Model/Peg.java
+++ src/Model/Peg.java
@@ -4,5 +4,30 @@ package Model;
  * Pegs are the pieces in the game that will be constantly moving
  * @author Yancarlos Diaz
  */
-public class Peg {
+public class Node {
+	Node [] adjNodes;
+	int isFilled;
+
+    public Node(){
+    	isFilled = 0;
+
+    }
+    public Node(int isFilled){
+    	    
+    }
+    public Node(int isFilled, Node [] adjacentNodes){
+    }
+    public Node(Node [] adjacentNode){
+    }
+
+    public int hasNode(){
+    	return this.isFilled;
+    }
+
+    public void setAdjacentNode(int index, Node newNode){
+    	this.adjNodes[index] = newNode;
+    }
+    
+    public void getAdjNode(int index){
+    	return this.adjNodes[index];
 }
