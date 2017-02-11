7032b20c27a832fdb66e01a81a8deff589da44a1
lshadler <lxs2208@rit.edu>
2017-02-11 15:35:09 -0500

    Created simple test function for model printing.

---
+++ src/Model/testModel.java
@@ -0,0 +1,12 @@
+package Model;
+
+/**
+ * @author lshadler
+ * Prints the model for testing
+ */
+public class testModel {
+    public static void main(String [] args){
+        Model test = new Model();
+        System.out.println(test.toString());
+    }
+}
