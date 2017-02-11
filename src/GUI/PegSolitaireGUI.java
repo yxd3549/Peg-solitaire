74136d513558c9b7d4fc577d686f4ca3e4e90bbf
lshadler <lxs2208@rit.edu>
2017-02-11 17:55:49 -0500

    Clear screen functionality.

--- src/GUI/PegSolitaireGUI.java
+++ src/GUI/PegSolitaireGUI.java
@@ -1,5 +1,7 @@
 package GUI;
 
+
+
 /**
  * PegSolitaireGUI is the Graphical User Interface of the game.
  * @author Yancarlos Diaz
--- src/PTUI/PegSolitairePTUI.java
+++ src/PTUI/PegSolitairePTUI.java
@@ -2,12 +2,13 @@ package PTUI;
 
 import Model.Model;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
  *
  */
-public class PegSolitairePTUI {
+public class PegSolitairePTUI{
 
     private Model model;
 
@@ -21,7 +22,18 @@ public class PegSolitairePTUI {
         System.out.print(controller.model);
         System.out.println("Select a Peg to remove:");
         controller.model.remove(s.nextInt());
+        String os = System.getProperty("os.name");
         while(true) {
+            try {
+                if (os.equals("windows")) Runtime.getRuntime().exec("cmd /c cls");
+                else {
+                    System.out.println("\u001b[2J \u001b[H");
+                    System.out.flush();
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
             System.out.print(controller.model);
 
             System.out.println("Select a Peg to move:");
