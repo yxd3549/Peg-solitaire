3f445d6b4c2e56372cc5d141cccc9a2c07d5849b
Michelle Zhou <myzhou96@gmail.com>
2017-02-12 08:29:48 -0500

    Documentation for Node

--- src/Model/Node.java
+++ src/Model/Node.java
@@ -1,28 +1,22 @@
 package Model;
 
 /**
- * Nodes represent the Pegs that will be constantly moving in the game.
+ * Nodes represent the places where a peg can be placed in the game.
  * @author Yancarlos Diaz
  * @author Luke Shadler
  * @author Bryan Camp
  * @author Michelle Zhou
  */
 public class Node {
-    /**
-     * An index of where the Node is in the board array.
-     */
+    /** The index where the Node is in the board array */
     private int index;
-    /**
-     * A list of adjacent Nodes
-     */
+    /** A list of adjacent Nodes */
     private Node[] adjNodes;
-    /**
-     * Boolean telling whether a Node has a Peg or not
-     */
+    /** Whether a Node has a Peg or not */
     private boolean hasPeg;
 
-    /**
-     * Public Copy constructor
+    /** Public Constructor
+     * @param n: Creates this node and sets up its adjacent nodes
      */
     public Node(Node n) {
         this.index = n.index;
@@ -38,9 +32,9 @@ public class Node {
         this.hasPeg = n.hasPeg;
     }
 
-    /**
-     * Public Constructor
-     * @param isFilled: if there is a peg here when initialized
+    /** Public Constructor
+     * @param isFilled: Whether there is a peg here when initialized
+     * @param index: Index of node in the board array
      */
     public Node(boolean isFilled, int index) {
         adjNodes = new Node[6];
@@ -48,10 +42,8 @@ public class Node {
         this.index = index;
     }
 
-    /**
-     * toString() method override
-     * @return Character based on what is contained in the node
-     */
+    /** toString() method override
+     * @return Character based on whether node contains a peg. */
     @Override
     public String toString() {
         if (this.hasPeg) {
@@ -61,21 +53,33 @@ public class Node {
         }
     }
 
+    /** Getter for hasPeg
+     * @return Whether the node contains a peg */
     public boolean isPeg() {
         return this.hasPeg;
     }
 
+    /** Getter for index
+     * @return The index in the board of this node */
     public int getIndex(){
         return this.index;
     }
 
+    /** Adds the given Node to the adjacent nodes.
+     * @param index: Index of node in board array
+     * @param newNode: Node that is adjacent to the node of index [index] in the board array */
     public void setAdjacentNode(int index, Node newNode) {
         this.adjNodes[index] = newNode;
     }
 
+    /** Sets the adjacent nodes.
+     * @param newNodes: Array of adjacent Nodes */
     public void setAdjacentNodes(Node[] newNodes){this.adjNodes = newNodes;}
 
-
+    /** Verifies the move the user wants to make.
+     * @param: index: Index of the spot in the board array you want to move to
+     * @return The index in the board array of the node that is being jumped over. [-1] of the move
+     * cannnot be made. */
     public int canMove(int index) {
         if (!hasPeg){
             return -1;
@@ -90,10 +94,12 @@ public class Node {
         return -1;
     }
 
+    /** Removes the peg on this node. */
     public void makeHole(){
         this.hasPeg = false;
     }
 
+    /** Puts a peg on this node. */
     public void makePeg(){
         this.hasPeg = true;
     }
